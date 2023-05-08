90d39d0842566158ad0d34937716d0c3d0f2f5cf
prashantmaurya228 <34316534+prashantmaurya228@users.noreply.github.com>
2023-05-09 00:58:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+function App() {
+  return (
+    <>
+      < Header />
+      <div className="app" />
+      <SimpleBottomNavigation />
+    </>
+  );
+}
+
+export default App;
