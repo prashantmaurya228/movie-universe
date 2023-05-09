a68c2e607480cea1818b88cfa4aaa289a10f7f9b
prashantmaurya228 <34316534+prashantmaurya228@users.noreply.github.com>
2023-05-10 05:26:32 +0530

    Delete src directory

--- src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Header.css'
-const Header = () => {
-    return (
-        <span className='header'>🎬Entertainment Hub🎬</span>
-    )
-}
-
-export default Header
